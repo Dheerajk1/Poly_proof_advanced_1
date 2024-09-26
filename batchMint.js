bebfd56528d7170d163353609d6df5a530a55284
Dheeraj K <132527159+Dheerajk1@users.noreply.github.com>
2024-09-26 18:16:58 +0530

    Update batchMint.js

--- batchMint.js
+++ batchMint.js
@@ -1,1 +1,40 @@
+// This script batch mints avengers ERC721A tokens.
 
+// Import required libraries
+const { ethers } = require("hardhat");
+require("dotenv").config();
+
+async function main() {
+  // Get private key from env
+  const privateKey = process.env.PRIVATE_KEY;
+
+  // The URL of the network provider
+  const networkAddress =
+    "https://ethereum-sepolia-rpc.publicnode.com";
+
+  // Create a provider using the URL
+  const provider = new ethers.providers.JsonRpcProvider(networkAddress);
+
+  // Create a signer from the private key and provider
+  const signer = new ethers.Wallet(privateKey, provider);
+
+  // Tthe address of the deployed contract
+  const contractAddress = "0x5Bbccf24daCe3549B31855F3D77Ce068F43389f4";
+
+  // Get the contract factory and attach it to the signer
+  const IndianNFT = await ethers.getContractFactory("avengers", signer);
+  const contract = await IndianNFT.attach(contractAddress);
+
+  // Call the mint function on the contract to mint 5 tokens
+  await contract.mint(5);
+
+  // Log a message to the console to indicate that the tokens have been minted
+  console.log("Minted 5 tokens");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
